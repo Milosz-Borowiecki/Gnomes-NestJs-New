867b3e62150fbc0a158a575e8c0c28cbc8708e2b
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-04-09 03:26:16 +0200

    Implement password hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UserLoggingDto } from 'src/auth/dtos/user-login.dto';
+import { randomBytes, scryptSync } from 'crypto';
 import { UserRegistrationDto } from 'src/auth/dtos/user-registration.dto';
 
 export type User = {
@@ -28,18 +29,18 @@ export class UserService {
             }
         }
 
-        var id: number = this.users.length;
+        const salt = randomBytes(16).toString('hex');
+        const hashedPassword = `${salt}:${scryptSync(userData.password,salt,64).toString('hex')}`;
+        const id: number = this.users.length;
 
         this.users.push({
             userId: id,
             username: userData.username,
-            password: userData.password
+            password: hashedPassword
         });
 
         return {
             message: "Account created successfully."
         }
-
     }
-
 }
