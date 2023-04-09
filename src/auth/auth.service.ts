3d258fc629c9ba4fa4b98a795a15e7d3ea5c20bf
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-04-10 01:13:12 +0200

    Create validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { UserRegistrationDto } from './dtos/user-registration.dto';
 import { UserLoggingDto } from './dtos/user-login.dto';
+import { scryptSync, timingSafeEqual } from 'crypto';
 
 @Injectable()
 export class AuthService {
     constructor(
         private userService: UserService
     ){}
+
+    async validateUser(loginData : UserLoggingDto) : Promise<any>{
+        const user = await this.userService.findOne(loginData.username);
+
+        const [salt, key] = user.password.split(':');
+        const hashedBuffer = scryptSync(loginData.password,salt,64);
+
+        const keyBuffer = Buffer.from(key,'hex');
+        const match = timingSafeEqual(hashedBuffer,keyBuffer);
+
+        if (user && match) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
 
     async register(dataUser: UserRegistrationDto) : Promise<any>{
 
@@ -18,7 +35,7 @@ export class AuthService {
 
     async login(loginData: UserLoggingDto) : Promise<any>{
 
-        let user = await this.userService.findOne(loginData);
+        let user = await this.userService.findOne(loginData.username);
 
         if(user === undefined){
             return {
