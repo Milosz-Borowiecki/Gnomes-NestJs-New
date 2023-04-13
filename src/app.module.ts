2718f209c8bacd0672e2701b552de3a1ba994f76
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-04-14 00:09:11 +0200

    Register JwtModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GnomesModule } from './gnomes/gnomes.module';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [UserModule, PassportModule,JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: {
+      expiresIn: process.env.EXPIRE_TIME
+    }
+  })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy,JwtStrategy]
 })
 export class AuthModule {}
