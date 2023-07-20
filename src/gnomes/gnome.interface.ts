7fbf580ec9e2ea84142e7bf07c35356073661263
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-07-20 23:32:45 +0200

    Update gnome interfaces

--- src/gnomes/gnome.interface.ts
+++ src/gnomes/gnome.interface.ts
@@ -1,10 +1,13 @@
 import { Races } from "./dtos/races";
 
-export interface Gnome {
-    gnomeId: number;
-    authorId:number;
+export interface CreateGnome {
     name: string;
     age: number;
     strength: number;
     race: Races;
+}
+
+export interface Gnome extends CreateGnome{
+    gnomeId: number;
+    authorId: number;
 }
