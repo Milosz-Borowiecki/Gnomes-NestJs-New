390faee8d2893a8a6bc116b64a84a073487a5604
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-08-20 01:20:02 +0200

    Rewrite update-gnome dto

--- src/gnomes/dtos/update-gnome.dto.ts
+++ src/gnomes/dtos/update-gnome.dto.ts
@@ -1,13 +1,35 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsString, Min, Max, IsEnum } from "class-validator";
-import { CreateGnomeDto } from "./create-gnome.dto";
+import { IsNumber, IsString, Min, Max, IsEnum, IsOptional } from "class-validator";
 import { Races } from "./races";
 
-export class UpdateGnomeDto extends CreateGnomeDto{
+export class UpdateGnomeDto{
 
     @ApiProperty()
+    @IsString()
+    @IsOptional()
+    name: string;
+
+    @ApiProperty()
+    @IsOptional()
     @IsNumber()
-    @Min(1)
-    id: number;
+    @Min(0)
+    @Max(100)
+    age: number;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    @Min(0)
+    @Max(150)
+    strength: number;
+
+    @ApiProperty(
+        {
+            enum: ['Sky', 'Rock', 'River','Fire','Forest']
+        }
+    )
+    @IsOptional()
+    @IsEnum(Races)
+    race: Races;
 
 }
