b017dce1cdbcd938962de734d790ce9748922191
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-08-31 20:17:44 +0200

    Fix database connection

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -15,9 +15,9 @@ dotenv.config();
       type: 'mysql',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
-      username: process.env.DB_DATABASE,
-      password: process.env.DB_USERNAME,
-      database: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [User,Gnome],
       synchronize: true,
     }),]
