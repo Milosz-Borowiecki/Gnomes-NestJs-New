0a41f9a6905ddb810c9b9a067690a33e710bb224
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-09-06 02:41:22 +0200

    Update create and modify endpoints

--- app/src/gnomes/gnomes.module.ts
+++ app/src/gnomes/gnomes.module.ts
@@ -3,9 +3,10 @@ import { GnomesService } from './gnomes.service';
 import { GnomesController } from './gnomes.controller';
 import { Gnome } from './entities/gnome.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Gnome])],
+  imports: [TypeOrmModule.forFeature([Gnome,User])],
   providers: [GnomesService],
   controllers: [GnomesController]
 })
--- app/src/gnomes/gnomes.service.ts
+++ app/src/gnomes/gnomes.service.ts
@@ -4,13 +4,16 @@ import { UpdateGnomeDto } from './dtos/update-gnome.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Gnome } from './entities/gnome.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class GnomesService {
 
     constructor(
         @InjectRepository(Gnome)
-        private gnomesRepository: Repository<Gnome>
+        private gnomesRepository: Repository<Gnome>,
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>
     ){}
 
     async findById(gnomeId: number) : Promise<Gnome | null>{
@@ -28,19 +31,36 @@ export class GnomesService {
         });
     }
 
-    create(body: CreateGnomeInterface,userId: number) : Promise<Gnome>{
+    async create(body: CreateGnomeInterface,userId: number) : Promise<Gnome>{
 
         const gnome = Object.assign(new Gnome(),body);
 
-        gnome.author_id = userId;
+        const user = await this.usersRepository.findOne({ 
+            where: { id: userId }, 
+            relations: ['gnomes']
+        });
+    
+        const newGnome = await this.gnomesRepository.save(gnome)
 
-        return this.gnomesRepository.save(gnome);
+        user.gnomes.push(gnome)
+
+        await this.usersRepository.save(user);
+
+        return newGnome;
     }
 
-    modify(gnomeId: number,body: UpdateGnomeDto) : Promise<Gnome>{
+    async modify(gnomeId: number,body: UpdateGnomeDto) : Promise<Gnome>{
 
         const gnome = Object.assign(new Gnome(),body);
 
+        const isExist = await this.gnomesRepository.exist({ 
+            where: { id: gnomeId } 
+        });
+
+        if(!isExist){
+            return;
+        }
+    
         gnome.id = gnomeId;
 
         return this.gnomesRepository.save(gnome);
