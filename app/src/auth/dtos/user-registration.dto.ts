75724887a5682bbde66f5cf300930b173cc66561
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-09-20 03:56:04 +0200

    Update user dtos and delete old user interface

--- app/src/auth/dtos/user-registration.dto.ts
+++ app/src/auth/dtos/user-registration.dto.ts
@@ -1,17 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UserRegistrationDto {
 
     @ApiProperty()
     @IsString()
     @MinLength(4)
+    @MaxLength(24)
     @IsNotEmpty()
     username: string;
 
     @ApiProperty()
     @IsString()
     @MinLength(8)
+    @MaxLength(250)
     @IsNotEmpty()
     password: string;
 }
